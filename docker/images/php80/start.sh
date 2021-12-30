5ab0940b98539e0ec937315572162ed8f7d45b13
cy322666 <cy322666@gmail.com>
2021-12-30 14:19:30 +0300

    install .env

--- docker/images/php80/start.sh
+++ docker/images/php80/start.sh
@@ -4,9 +4,11 @@ PHP=/usr/local/bin/php
 
 composer install
 
+cp ./.env.example ./.env
+
 php artisan key:generate
 php artisan migrate
-php artisan orchid:install
+#php artisan orchid:install
 php artisan orchid:admin root soft@gigwork.ru jsd**384378JJK
 
 php-fpm
